e8e3307760f53a2346c1e385abb898207fcda8a2
이하성 <tedsoftj1123@gmail.com>
2022-09-18 23:40:52 +0900

    📑 :: runner

--- src/main/java/com/example/messenger/websocket/SocketRunner.java
+++ src/main/java/com/example/messenger/websocket/SocketRunner.java
@@ -1,10 +1,14 @@
 package com.example.messenger.websocket;
 
+import com.corundumstudio.socketio.SocketIOServer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 
+@RequiredArgsConstructor
 public class SocketRunner implements CommandLineRunner {
+    private final SocketIOServer socketIOServer;
     @Override
     public void run(String... args) throws Exception {
-
+        socketIOServer.start();
     }
 }
