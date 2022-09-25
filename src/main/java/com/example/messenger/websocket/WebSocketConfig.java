437bcc0493592abbc07c9496a8ad73a4d7fbea8f
이하성 <tedsoftj1123@gmail.com>
2022-09-25 23:04:24 +0900

    .

--- src/main/java/com/example/messenger/websocket/WebSocketConfig.java
+++ src/main/java/com/example/messenger/websocket/WebSocketConfig.java
@@ -1,34 +1,13 @@
 package com.example.messenger.websocket;
 
-import com.corundumstudio.socketio.SocketConfig;
-import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-public class WebSocketConfig {
-    @Value("${socket.port}")
-    private Integer port;
-
-    @Bean
-    public SocketIOServer socketIOServer() {
-        com.corundumstudio.socketio.Configuration configuration = new com.corundumstudio.socketio.Configuration();
-        SocketConfig socketConfig = new SocketConfig();
-        socketConfig.setReuseAddress(true);
-        socketConfig.setTcpNoDelay(true);
-
-        configuration.setPort(port);
-        configuration.setHostname("localhost");
-        configuration.setSocketConfig(socketConfig);
-        configuration.setOrigin("*");
-
-        return new SocketIOServer(configuration);
-    }
-
-    @Bean
-    public SpringAnnotationScanner springAnnotationScanner() {
-        return new SpringAnnotationScanner(socketIOServer());
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/")
     }
 }
